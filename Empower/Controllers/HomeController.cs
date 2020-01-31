4f27ee27cfbca8c77a37ba2201c10e1487be0fe0
Craig.MacIvor <Craig.MacIvor@richmondgov.com>
2020-01-31 16:40:48 -0500

    make app title dynamic

--- Empower/Controllers/HomeController.cs
+++ Empower/Controllers/HomeController.cs
@@ -38,6 +38,8 @@ namespace Empower.Controllers
                 //AuthenticationResponse
             };
 
+            viewModel.GetAppTitle();
+
             return View(viewModel);
         }
 
--- Empower/Models/ViewModelBase.cs
+++ Empower/Models/ViewModelBase.cs
@@ -1,9 +1,26 @@
 using Empower.Authentication;
+using System;
 
 public abstract class ViewModelBase 
 {
 
     public string UserName { get; set; }
     public string  SystemID { get; set; }
+
+    public string AppTitle { get; set; }
+
+    public void GetAppTitle()
+    {
+        int systemID = Convert.ToInt32(SystemID);
+
+        if (systemID == (int) AppVersion.Adult || systemID == (int)AppVersion.Juvenile)
+        {
+            AppTitle = "EMPOWER Referral for Services";
+        }
+        if (systemID == (int) AppVersion.CWB)
+        {
+            AppTitle = "Office of Community Wealth Building";
+        }
+    }
 
 }
