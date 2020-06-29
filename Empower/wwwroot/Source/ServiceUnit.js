63bde7286f67aa09282c15d2f31ee96872db5d86
Craig.MacIvor <Craig.MacIvor@richmondgov.com>
2020-06-29 10:30:49 -0400

    add validation

--- Empower/wwwroot/Source/ServiceUnit.js
+++ Empower/wwwroot/Source/ServiceUnit.js
@@ -7,93 +7,125 @@ import { toggleServiceUnitModal } from './EnrollmentTabHelpers';
 import {triggerToastMessage, triggerErrorMessage  } from './ToastHelper';
 
 
-function ddlServiceUnitYearSelectHandler(event) {
-    let selectedValue = event.currentTarget.getAttribute('value');
-    //console.log(selectedValue);
+const ServiceUnit = (props) => {
 
-    document.getElementById('btnServiceYear').value = selectedValue;
-    document.getElementById("btnServiceYear").innerHTML = selectedValue;
-}
+    useEffect(() => {
+        document.getElementById('btnServiceYear').value = "Please Select";
+        document.getElementById("btnServiceYear").innerHTML = "Please Select";
 
-function ddlServiceUnitMonthSelectHandler(event) {
-    let selectedValue = event.currentTarget.getAttribute('value');
+        document.getElementById('btnServiceMonth').value = "Please Select";
+        document.getElementById("btnServiceMonth").innerHTML = "Please Select";
 
-    document.getElementById('btnServiceMonth').value = selectedValue;
-    document.getElementById("btnServiceMonth").innerHTML = selectedValue;
-}
+    });
 
-let serviceUnitYears = [];
-let currentYear = new Date().getFullYear();
-let oldestYear = currentYear - 20;
-// console.log("current year: " + currentYear);
-// console.log("oldest: " + oldestYear);
 
-var i;
-for (i = 0; i < 20; i++) {
-    oldestYear++;
-    serviceUnitYears.push(oldestYear);
-}
-
-function saveServiceUnit() {
-
-    let apiAddress = sessionStorage.getItem("baseApiAddress");
-    let fullServiceUnitAddress = `${apiAddress}/api/ServiceUnit`;
-    let sessionStorageData = getSessionData();
-
-    let serviceUnit = {
-        EnrollmentID: $("#hdnServiceUnitEnrollmentID").val(),
-        Year: document.getElementById('btnServiceYear').value,
-        Month: document.getElementById('btnServiceMonth').value,
-        Units: $("#txtServiceUnits").val(),
-        SystemID: sessionStorageData.SystemID,
-        Active: true,
-        UpdatedDate: new Date(),
-        UpdatedBy: sessionStorageData.CurrentUser
+    function ddlServiceUnitYearSelectHandler(event) {
+        let selectedValue = event.currentTarget.getAttribute('value');
+        //console.log(selectedValue);
+    
+        document.getElementById('btnServiceYear').value = selectedValue;
+        document.getElementById("btnServiceYear").innerHTML = selectedValue;
     }
-
-    let methodType = '';
-
-    if ($("#hdnServiceUnitID").val() !== "") {
-        methodType = 'PUT';
-        serviceUnit.CreatedDate = new Date($("#hdnServiceUnitCreatedDate").val());
-        serviceUnit.CreatedBy = $("#hdnServiceUnitCreatedBy").val();
-
-    } else {
-        methodType = 'POST';
-        serviceUnit.CreatedDate = new Date();
-        serviceUnit.CreatedBy = sessionStorageData.CurrentUser;
+    
+    function ddlServiceUnitMonthSelectHandler(event) {
+        let selectedValue = event.currentTarget.getAttribute('value');
+    
+        document.getElementById('btnServiceMonth').value = selectedValue;
+        document.getElementById("btnServiceMonth").innerHTML = selectedValue;
     }
+    
+    let serviceUnitYears = [];
+    serviceUnitYears.push('Please Select');
+    let currentYear = new Date().getFullYear();
+    let oldestYear = currentYear - 20;
+    // console.log("current year: " + currentYear);
+    // console.log("oldest: " + oldestYear);
+    
+    var i;
+    for (i = 0; i < 20; i++) {
+        oldestYear++;
+        serviceUnitYears.push(oldestYear);
+    }
+    
+    function saveServiceUnit() {
 
-    fetch(fullServiceUnitAddress, {
-        method: methodType,
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + sessionStorageData.Token
-        },
-        body: JSON.stringify(serviceUnit)
-    }).then(result => result.json())
-    .then(result => {
-        console.log(result);
+        let serviceYear = document.getElementById('btnServiceYear').value;
+        let serviceMonth = document.getElementById('btnServiceMonth').value;
+        let units = $("#txtServiceUnits").val();
 
-        if (result === null || result.Message !== undefined) {
-            props.createErrorNotification("an error occurred while saving the record.");
+        if (serviceYear === "Please Select") {
+            document.getElementById("divServiceYears").removeAttribute("style");
             return;
         }
 
-        //toggleServiceUnitModal();
+        if (serviceMonth === "Please Select") {
+            document.getElementById("divServiceMonth").removeAttribute("style");
+            return;
+        }
 
-        triggerToastMessage("The service unit was successfully saved");
-
-    });
-}
-
-
-let serviceUnitYearOptions = serviceUnitYears.map((value) =>
-    <a key={value} value={value}  onClick={ ddlServiceUnitYearSelectHandler  } className="dropdown-item">{value}</a>
- );
+        if (units === "") {
+            $("#frmServiceUnit").addClass("was-validated");
+            return;
+        }
 
 
-const ServiceUnit = (props) => {
+        let apiAddress = sessionStorage.getItem("baseApiAddress");
+        let fullServiceUnitAddress = `${apiAddress}/api/ServiceUnit`;
+        let sessionStorageData = getSessionData();
+    
+        let serviceUnit = {
+            EnrollmentID: $("#hdnServiceUnitEnrollmentID").val(),
+            Year: document.getElementById('btnServiceYear').value,
+            Month: document.getElementById('btnServiceMonth').value,
+            Units: $("#txtServiceUnits").val(),
+            SystemID: sessionStorageData.SystemID,
+            Active: true,
+            UpdatedDate: new Date(),
+            UpdatedBy: sessionStorageData.CurrentUser
+        }
+    
+        let methodType = '';
+    
+        if ($("#hdnServiceUnitID").val() !== "") {
+            methodType = 'PUT';
+            serviceUnit.CreatedDate = new Date($("#hdnServiceUnitCreatedDate").val());
+            serviceUnit.CreatedBy = $("#hdnServiceUnitCreatedBy").val();
+    
+        } else {
+            methodType = 'POST';
+            serviceUnit.CreatedDate = new Date();
+            serviceUnit.CreatedBy = sessionStorageData.CurrentUser;
+        }
+    
+        fetch(fullServiceUnitAddress, {
+            method: methodType,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + sessionStorageData.Token
+            },
+            body: JSON.stringify(serviceUnit)
+        }).then(result => result.json())
+        .then(result => {
+            console.log(result);
+    
+            if (result === null || result.Message !== undefined) {
+                props.createErrorNotification("an error occurred while saving the record.");
+                return;
+            }
+    
+            //toggleServiceUnitModal();
+    
+            triggerToastMessage("The service unit was successfully saved");
+    
+        });
+    }
+    
+    
+    let serviceUnitYearOptions = serviceUnitYears.map((value) =>
+        <a key={value} value={value}  onClick={ ddlServiceUnitYearSelectHandler  } className="dropdown-item">{value}</a>
+     );
+
+
     return <div>
         <input type="hidden" id="hdnServiceUnitID" />
         <input type="hidden" id="hdnServiceUnitEnrollmentID" />
@@ -118,6 +150,7 @@ const ServiceUnit = (props) => {
                                             
                                         </button>
                                         <div className="dropdown-menu">
+                                           <option value="Please Select" onClick={ddlServiceUnitMonthSelectHandler} className="dropdown-item">Please Select</option>
                                            <option value="1" onClick={ddlServiceUnitMonthSelectHandler} className="dropdown-item">JAN</option>
                                            <option value="2" onClick={ddlServiceUnitMonthSelectHandler} className="dropdown-item">FEB</option>
                                            <option value="3" onClick={ddlServiceUnitMonthSelectHandler} className="dropdown-item">MAR</option>
@@ -132,7 +165,7 @@ const ServiceUnit = (props) => {
                                            <option value="12" onClick={ddlServiceUnitMonthSelectHandler} className="dropdown-item">DEC</option>
                                         </div>
                                     </div>
-                                    {/* <div style={{display:'none'}} id="divServiceMonth" className='errorDiv'>Please select a value.</div>  */}
+                                    <div style={{display:'none'}} id="divServiceMonth" className='errorDiv'>Please select a value.</div> 
                                 </div>
                             </div>
                             <div className="form-row">
@@ -146,12 +179,13 @@ const ServiceUnit = (props) => {
                                            {serviceUnitYearOptions }
                                         </div>
                                     </div>
+                                    <div style={{display:'none'}} id="divServiceYears" className='errorDiv'>Please select a value.</div>
                                 </div>
                             </div>
                             <div className="form-row">
                                 <div className="col-4">
                                     <label htmlFor="txtServiceUnits"><strong> Units *</strong></label>
-                                    <input type="text" className="form-control" id="txtServiceUnits" />
+                                    <input type="text" className="form-control" id="txtServiceUnits" defaultValue="" required />
                                 </div>
                             </div>
                             <div id="divServiceUnitsTableContainer" className="form-row">
