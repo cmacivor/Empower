79f82051d2b22d74f1f94e95f71872090f90d8ac
Craig.MacIvor <Craig.MacIvor@richmondgov.com>
2020-07-02 13:48:48 -0400

    modal work

--- Empower/wwwroot/Source/EnrollmentCaseModal.js
+++ Empower/wwwroot/Source/EnrollmentCaseModal.js
@@ -19,7 +19,7 @@ const EnrollmentCaseModal = (props) => {
     return <div>
         <h3>Referral</h3>
         <br/>
-        <button id="btnAddCaseEnrollment" onClick={toggleCaseEnrollmentModal} >Add Case Info</button>
+        <button id="btnAddCaseEnrollment" className="btn btn-primary" onClick={toggleCaseEnrollmentModal} >Add Case Info</button>
               <form id="frmCaseEnrollment">
                 <div className="modal fade" id="caseEnrollmentModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div className="modal-dialog modal-lg" role="document">
@@ -36,8 +36,9 @@ const EnrollmentCaseModal = (props) => {
                                         <label htmlFor="txtCourtOrderDate"><strong>Court Order Date</strong></label>
                                         <input type="date" defaultValue="" id="txtCourtOrderDate" className="form-control"></input>
                                     </div>
-                                    <div className="col-4">
+                                    <div className="col-6">
                                         <div className="dropdown">
+                                            <label htmlFor="btnOverallRisk"><strong>Overall Risk at Time of Placement *</strong></label>
                                             <button type="button" id="btnOverallRisk" value="" className="btn btn-primary dropdown-toggle" data-toggle="dropdown">
                                                 
                                             </button>
@@ -50,10 +51,11 @@ const EnrollmentCaseModal = (props) => {
                                 </div>
                                 <div className="form-row">
                                     <div className="col-4">
-                                        <label htmlFor="txtNextCourtDate"><strong>Next Cuort Date</strong></label>
+                                        <label htmlFor="txtNextCourtDate"><strong>Next Court Date</strong></label>
                                         <input type="date" defaultValue="" id="txtNextCourtDate" className="form-control"></input>
                                     </div>
-                                    <div className="col-4">
+                                    <div className="col-6">
+                                        <label htmlFor="btnJudge"><strong>Judge *</strong></label>
                                         <div className="dropdown">
                                             <button type="button" id="btnJudge" value="" className="btn btn-primary dropdown-toggle" data-toggle="dropdown">
                                                 
@@ -64,6 +66,18 @@ const EnrollmentCaseModal = (props) => {
                                         </div>
                                         <div style={{display:'none'}} id="divJudgeError" className='errorDiv'>Please select a value.</div> 
                                     </div>
+                                </div>
+                                <div className="form-row">
+                                    <label htmlFor="txtPlacementCharges"><strong>Placement Charges *</strong></label>
+                                    <input id="txtPlacementCharges" className="form-control" />
+                                </div>
+                                <div id="divPlacementChargesContainer">
+
+                                </div>
+                                <br/>
+                                <div className="form-row">
+                                    <label><strong>Court Order Narrative</strong></label>
+                                    <input id="txtCourtOrderNarrative" className="form-control" />
                                 </div>
                             </div>
                             <div className="modal-footer">
